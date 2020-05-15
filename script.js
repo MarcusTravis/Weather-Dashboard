c3b36321e2048db50230b291246cbd671f3cf264
Marcus <mpt789@gmail.com>
2020-05-15 01:07:33 -0700

    added jQuery Ajax calls, one for weather infor, one for UV info. Also deleted style.css since bootstrap allows me to no longer require it.

--- script.js
+++ script.js
@@ -8,46 +8,107 @@
     //use local storage to keep data inside divs or spans
     //button needs to make call to weather API
     var leftCol = document.getElementById("leftCol");
+    var rightCol = document.getElementById("rightCol");
     var input = document.createElement("input");
-    var div = document.createElement("div")
-    var btn1 = document.createElement("button")
-    leftCol.textContent = ("Search for a city:");
-    leftCol.setAttribute("class", "h3 pl-5")
-    leftCol.appendChild(div);
-    div.appendChild(input);
-    input.setAttribute("class", "mt-3")
-    div.appendChild(btn1);
-    btn1.setAttribute("class", "fa fa-search 3x p-1 m-0 ml-3")
+    var div1 = document.createElement("div");
+    var div2 = document.createElement("div");
+    var div3 = document.createElement("div");
+    var cityInfoSpan = document.createElement("span");
+    var tempSpan = document.createElement("span");
+    var humidSpan = document.createElement("span");
+    var windSpan = document.createElement("span");
+    var UVSpan = document.createElement("span");
+    var linebreak = document.createElement("br");
+    // var div4 = document.createElement("div")
+    var btn1 = document.createElement("button");
 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=Gilbert,Arizona&appid=a2c6aeb70811d753296d3acafec7dceb";
+    leftCol.setAttribute("class", "h2 pl-5");
+    leftCol.appendChild(div1);
+    leftCol.appendChild(div2);
+    rightCol.appendChild(div3);
+    
+    div1.textContent = ("Search for a city:");
+    div1.setAttribute("class", "h1");
 
-      $.ajax({
-        url: queryURL,
+
+    div2.appendChild(input);
+    input.setAttribute("class", "mt-3");
+    input.setAttribute("placeholder", "Search...");
+    div2.appendChild(btn1);
+    btn1.setAttribute("class", "fa fa-search 3x bg-primary rounded border p-1 m-0 ml-1");
+    
+    div3.appendChild(cityInfoSpan);
+    cityInfoSpan.innerText = ("Citay");
+    cityInfoSpan.setAttribute("class", "h1 navbar");
+
+    
+    btn1.addEventListener("click", function(e){
+        var inputVal = input.value;
+        console.log(inputVal)
+        
+    });
+    
+    
+    var queryURL1 = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q="+ inputVal + "&appid=a2c6aeb70811d753296d3acafec7dceb";
+
+    
+    $.ajax({
+        url: queryURL1,
         method: "GET"
-      })
-      .then(function(response) {
-          var results = response.data;
-          console.log(results);
-          console.log(response);
-          console.log(response.weather);
-          
-        });
+    })
+    .then(function(response) {
+        var results = response.data;
+        const temp = response.main.temp;
+        const wind = response.wind.speed;
+        const humid = response.main.humidity;
+        console.log(response);
+        
+        
+        
+        div3.appendChild(tempSpan);
+        tempSpan.innerText = ("Tempurature: " + temp);
+        tempSpan.setAttribute("class", "h4 mt-3 navbar");
+        
+        div3.appendChild(humidSpan);
+        humidSpan.textContent = ("Humidity: " + humid);
+        humidSpan.setAttribute("class", "h4 mt-3 navbar");
+        
+        div3.appendChild(windSpan);
+        windSpan.textContent = ("Wind Speed: " + wind);
+        windSpan.setAttribute("class", "h4 mt-3 navbar");
+        
+        
+    });
+    
+    var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=a2c6aeb70811d753296d3acafec7dceb&lat=33.35&lon=-111.79";
+    
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+    })
+    .then(function(response2) {
+        console.log(response2)
+        const UV = response2.value
+        
 
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+        div3.appendChild(UVSpan);
+        UVSpan.textContent = ("UV Index: " + UV);
+        UVSpan.setAttribute("class", "h4 mt-3 navbar");
+    });
+    // WHEN I view current weather conditions for that city
+    // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
     //display info retrieved for API
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+    
+    // WHEN I view the UV index
+    // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
     //set class attributes to properly style uv index div using bootstrap
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// WHEN I open the weather dashboard
+    
+    // WHEN I view future weather conditions for that city
+    // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+    
+    
+    // WHEN I click on a city in the search history
+    // THEN I am again presented with current and future conditions for that city
+    
+    // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
