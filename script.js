437b757b2930a66bdf6059f9e0e5ee0f2c71539f
Marcus <mpt789@gmail.com>
2020-05-17 13:20:49 -0700

    added uv index background and bg color depending on how high uv index is

--- script.js
+++ script.js
@@ -17,88 +17,111 @@
     var tempSpan = document.createElement("span");
     var humidSpan = document.createElement("span");
     var windSpan = document.createElement("span");
-    var UVSpan = document.createElement("span");
+    var UVSpan = document.createElement("div");
     var linebreak = document.createElement("br");
     // var div4 = document.createElement("div")
     var btn1 = document.createElement("button");
-
-    leftCol.setAttribute("class", "h2 pl-5");
+    
+    leftCol.setAttribute("class", "h2 pl-5 pr-5 pb-5  border rounded bg-light");
     leftCol.appendChild(div1);
     leftCol.appendChild(div2);
     rightCol.appendChild(div3);
     
     div1.textContent = ("Search for a city:");
     div1.setAttribute("class", "h1");
-
+    
 
     div2.appendChild(input);
     input.setAttribute("class", "mt-3");
     input.setAttribute("placeholder", "Search...");
+    input.setAttribute("id", "input");
     div2.appendChild(btn1);
     btn1.setAttribute("class", "fa fa-search 3x bg-primary rounded border p-1 m-0 ml-1");
     
     div3.appendChild(cityInfoSpan);
-    cityInfoSpan.innerText = ("Citay");
-    cityInfoSpan.setAttribute("class", "h1 navbar");
+    
+    
+    btn1.addEventListener("click", getInput) 
+    
+        function getInput() {
 
-    
-    // btn1.addEventListener("click", getLocalData() {
-    //     const city = input.value;
-
-    // });
-    
-    
-    
-    
-    // const queryURL1 = `http://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=a2c6aeb70811d753296d3acafec7dceb`
-    // console.log(queryURL1)
-    var queryURL1 = "http://api.openweathermap.org/data/2.5/weather?units=imperial&q=Gilbert,Arizona&appid=a2c6aeb70811d753296d3acafec7dceb";
-    $.ajax({
-        url: queryURL1,
-        method: "GET"
-    })
-    .then(function(result) {
-            const response = result.data
-            const temp = response.main.temp;
-            const wind = response.wind.speed;
-            const humid = response.main.humidity;
-            console.log(response);
+            let inputVal = document.getElementById("input").value;
+            const city = inputVal;
             
+            console.log(city);
+            
+            
+            
+            const queryURL1 = `http://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=a2c6aeb70811d753296d3acafec7dceb`
+            $.ajax({
+                url: queryURL1,
+                method: "GET"
+            })
+            .then(function(response) {
+
+                const temp = response.main.temp;
+                const wind = response.wind.speed;
+                const humid = response.main.humidity;
+                const lat = response.coord.lat
+                const lon = response.coord.lon
+                // console.log(response);
                 
-            
-            
-            div3.appendChild(tempSpan);
-            tempSpan.innerText = ("Tempurature: " + temp);
-            tempSpan.setAttribute("class", "h4 mt-3 navbar");
-            
-            div3.appendChild(humidSpan);
-            humidSpan.textContent = ("Humidity: " + humid);
-            humidSpan.setAttribute("class", "h4 mt-3 navbar");
-            
-            div3.appendChild(windSpan);
-            windSpan.textContent = ("Wind Speed: " + wind);
-            windSpan.setAttribute("class", "h4 mt-3 navbar");
-        });
-  
-            
-        
-        
-    function getUV(lat, lon) {
-        var queryURL2 = `http://api.openweathermap.org/data/2.5/uvi?appid=a2c6aeb70811d753296d3acafec7dceb&lat=${lat}&lon=${lon}`;
-        $.ajax({
-            url: queryURL2,
-            method: "GET"
-        })
-        .then(function(response) {
-            console.log(response)
-            const UV = response.value
-            
-            
-            div3.appendChild(UVSpan);
-            UVSpan.textContent = ("UV Index: " + UV);
-            UVSpan.setAttribute("class", "h4 mt-3 navbar");
-        });
-    }
+                div3.setAttribute("class", "border rounded")
+                cityInfoSpan.innerText = (city);
+                cityInfoSpan.setAttribute("class", "h1 navbar bg-light")
+
+                div3.appendChild(tempSpan);
+                tempSpan.innerText = ("Tempurature: " + temp);
+                tempSpan.setAttribute("class", "h4 pt-3 rounded bg-white navbar");
+                
+                div3.appendChild(humidSpan);
+                humidSpan.textContent = ("Humidity: " + humid);
+                humidSpan.setAttribute("class", "h4 pt-3 navbar bg-light");
+                
+                div3.appendChild(windSpan);
+                windSpan.textContent = ("Wind Speed: " + wind);
+                windSpan.setAttribute("class", "h4 pt-3 rounded bg-white navbar");
+                
+                
+                
+                
+                var queryURL2 = `http://api.openweathermap.org/data/2.5/uvi?appid=a2c6aeb70811d753296d3acafec7dceb&lat=${lat}&lon=${lon}`;
+                $.ajax({
+                    url: queryURL2,
+                    method: "GET"
+                })
+                .then(function(response) {
+                    // console.log(response)
+                    const UV = response.value
+                    
+                    //Creates button to display UV value form API
+                    var uvButton = document.createElement("button");
+                    uvButton.textContent = (UV);
+                    //Adds UV span inside div3
+                    div3.appendChild(UVSpan);
+                    //Adds content inside UV span
+                    UVSpan.textContent = ("UV Index: ");
+                    UVSpan.setAttribute("class", "h4 pt-3 pl-3 rounded bg-light");
+                    //Adds uv button that displays uv value
+                    UVSpan.appendChild(uvButton);
+                    uvButton.setAttribute("class", "rounded")
+
+                    //colors uv button background depending on how high or low uv index is.
+                    if (response.value > 0) {
+                        uvButton.setAttribute("class", "bg-success rounded");
+                    }
+                    if (response.value > 3) {
+                        uvButton.setAttribute("class", "bg-warning rounded");
+                    }
+                    if (response.value > 7) {
+                        uvButton.setAttribute("class", "bg-danger rounded");
+                    }
+                    
+                });
+            });
+        }
+
+
         // WHEN I view current weather conditions for that city
     // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
     //display info retrieved for API
