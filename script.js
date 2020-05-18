502eb0fce0934b7427e20a5c2cf0bd8bdde0b22d
Marcus <mpt789@gmail.com>
2020-05-17 19:39:48 -0700

    added 5 day forecast!

--- script.js
+++ script.js
@@ -13,19 +13,20 @@
     var div1 = document.createElement("div");
     var div2 = document.createElement("div");
     var div3 = document.createElement("div");
+    var div4 = document.createElement("div")
     var cityInfoSpan = document.createElement("span");
     var tempSpan = document.createElement("span");
     var humidSpan = document.createElement("span");
     var windSpan = document.createElement("span");
     var UVSpan = document.createElement("div");
     var linebreak = document.createElement("br");
-    // var div4 = document.createElement("div")
     var btn1 = document.createElement("button");
     
     leftCol.setAttribute("class", "h2 pl-5 pr-5 pb-5  border rounded bg-light");
     leftCol.appendChild(div1);
     leftCol.appendChild(div2);
     rightCol.appendChild(div3);
+    rightCol.appendChild(div4);
     
     div1.textContent = ("Search for a city:");
     div1.setAttribute("class", "h1");
@@ -57,29 +58,32 @@
                 url: queryURL1,
                 method: "GET"
             })
-            .then(function(response) {
+            .then(function(response1) {
 
-                const temp = response.main.temp;
-                const wind = response.wind.speed;
-                const humid = response.main.humidity;
-                const lat = response.coord.lat
-                const lon = response.coord.lon
-                // console.log(response);
+                const temp = response1.main.temp;
+                const wind = response1.wind.speed;
+                const humid = response1.main.humidity;
+                const lat = response1.coord.lat
+                const lon = response1.coord.lon
+                console.log("Today's Weather response:");
+                console.log(response1);
+
+                const m = moment();
                 
                 div3.setAttribute("class", "border rounded")
-                cityInfoSpan.innerText = (city);
+                cityInfoSpan.innerText = (city + ",   " + m.format("MMMM Do YYYY"));
                 cityInfoSpan.setAttribute("class", "h1 navbar bg-light")
 
                 div3.appendChild(tempSpan);
-                tempSpan.innerText = ("Tempurature: " + temp);
+                tempSpan.innerText = ("Tempurature: " + temp + "\xB0 F");
                 tempSpan.setAttribute("class", "h4 pt-3 rounded bg-white navbar");
                 
                 div3.appendChild(humidSpan);
-                humidSpan.textContent = ("Humidity: " + humid);
-                humidSpan.setAttribute("class", "h4 pt-3 navbar bg-light");
+                humidSpan.textContent = ("Humidity: " + humid + "%");
+                humidSpan.setAttribute("class", "h4 pt-3 pb-3 navbar bg-light");
                 
                 div3.appendChild(windSpan);
-                windSpan.textContent = ("Wind Speed: " + wind);
+                windSpan.textContent = ("Wind Speed: " + wind + " mph");
                 windSpan.setAttribute("class", "h4 pt-3 rounded bg-white navbar");
                 
                 
@@ -90,9 +94,10 @@
                     url: queryURL2,
                     method: "GET"
                 })
-                .then(function(response) {
-                    // console.log(response)
-                    const UV = response.value
+                .then(function(response2) {
+                    console.log("UV Index response:")
+                    console.log(response2)
+                    const UV = response2.value
                     
                     //Creates button to display UV value form API
                     var uvButton = document.createElement("button");
@@ -101,27 +106,89 @@
                     div3.appendChild(UVSpan);
                     //Adds content inside UV span
                     UVSpan.textContent = ("UV Index: ");
-                    UVSpan.setAttribute("class", "h4 pt-3 pl-3 rounded bg-light");
+                    UVSpan.setAttribute("class", "h4 pt-3 pl-3 pb-3 mb-0 rounded bg-light");
                     //Adds uv button that displays uv value
                     UVSpan.appendChild(uvButton);
                     uvButton.setAttribute("class", "rounded")
 
                     //colors uv button background depending on how high or low uv index is.
-                    if (response.value > 0) {
+                    if (response2.value > 0) {
                         uvButton.setAttribute("class", "bg-success rounded");
                     }
-                    if (response.value > 3) {
+                    if (response2.value > 4) {
                         uvButton.setAttribute("class", "bg-warning rounded");
                     }
-                    if (response.value > 7) {
+                    if (response2.value > 8) {
                         uvButton.setAttribute("class", "bg-danger rounded");
                     }
+
+
+                        var queryURL3 = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${city}&appid=a2c6aeb70811d753296d3acafec7dceb`;
+                    $.ajax({
+                        url: queryURL3,
+                        method: "GET"
+                    })
+                    .then(function(response3) {
+                    
+                        console.log("5 day forecast response:");
+                        console.log(response3);
+
+                        const day1 = document.createElement("div");
+                        const day2 = document.createElement("div");
+                        const day3 = document.createElement("div");
+                        const day4 = document.createElement("div");
+                        const day5 = document.createElement("div");
+
+                        div4.appendChild(day1);
+                        div4.appendChild(day2);
+                        div4.appendChild(day3);
+                        div4.appendChild(day4);
+                        div4.appendChild(day5);
+                        div4.setAttribute("class", "mt-5")
+
+                        
+                        const day1Icon = response3.list[4].weather[0].description;
+                        const day2Icon = response3.list[12].weather[0].description;
+                        const day3Icon = response3.list[20].weather[0].description;
+                        const day4Icon = response3.list[28].weather[0].description;
+                        const day5Icon = response3.list[36].weather[0].description;
+                        
+                        const day1temp = response3.list[4].main.temp
+                        const day2temp = response3.list[12].main.temp
+                        const day3temp = response3.list[20].main.temp
+                        const day4temp = response3.list[28].main.temp
+                        const day5temp = response3.list[36].main.temp
+                        
+                        const day1humidity = response3.list[4].main.humidity
+                        const day2humidity = response3.list[12].main.humidity
+                        const day3humidity = response3.list[20].main.humidity
+                        const day4humidity = response3.list[28].main.humidity
+                        const day5humidity = response3.list[36].main.humidity
+                        
+                        
+                        day1.innerText = (m.add(1, 'days').format("MMMM Do YYYY") + "\r\n" + "" + day1Icon + "\r\n" + "Temp: " + Math.round(day1temp) + "\xB0 F" + "\r\n" + "Humidity: " +  day1humidity + "%");
+                        day1.setAttribute("class", "d-inline-block h4 m-1 ml-2 p-3 bg-primary rounded text-white")
+                        
+                        day2.innerText = (m.add(1, 'days').format("MMMM Do YYYY") + "\r\n" + "" + day2Icon + "\r\n" + "Temp: " + Math.round(day2temp) + "\xB0 F" + "\r\n" + "Humidity: " +  day2humidity + "%");
+                        day2.setAttribute("class", "d-inline-block h4 m-1 p-3 bg-primary rounded text-white")
+                        
+                        day3.innerText = (m.add(1, 'days').format("MMMM Do YYYY") + "\r\n" + "" + day3Icon + "\r\n" + "Temp: " + Math.round(day3temp) + "\xB0 F" + "\r\n" + "Humidity: " +  day3humidity + "%");
+                        day3.setAttribute("class", "d-inline-block h4 m-1 p-3 bg-primary rounded text-white")
+                        
+                        day4.innerText = (m.add(1, 'days').format("MMMM Do YYYY") + "\r\n" + "" + day4Icon + "\r\n" + "Temp: " + Math.round(day4temp) + "\xB0 F" + "\r\n" + "Humidity: " +  day4humidity + "%");
+                        day4.setAttribute("class", "d-inline-block h4 m-1 p-3 bg-primary rounded text-white")
+                        
+                        day5.innerText = (m.add(1, 'days').format("MMMM Do YYYY") + "\r\n" + "" + day5Icon + "\r\n" + "Temp: " + Math.round(day5temp) + "\xB0 F" + "\r\n" + "Humidity: " +  day5humidity + "%");
+                        day5.setAttribute("class", "d-inline-block h4 m-1 p-3 bg-primary rounded text-white") + "%"
+                        
+                        
+                        
+                        
+                    });  
                     
                 });
             });
         }
-
-
         // WHEN I view current weather conditions for that city
     // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
     //display info retrieved for API
