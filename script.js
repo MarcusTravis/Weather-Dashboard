6bc7fa700da3429f25c120cbfb69d3abfe10f99b
Marcus <mpt789@gmail.com>
2020-06-07 21:27:23 -0700

    took out unnecessary notes in script.js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-//using html, create divs needed for acceptance critera
-
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-//make an input section for html to retrieve info for city
-//create onclick function for input button
-//use local storage to keep data inside divs or spans
-//button needs to make call to weather API
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-//display info retrieved for API
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-//set class attributes to properly style uv index div using bootstrap
-
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
 
 //Main body
 const leftCol = document.getElementById("leftCol");
@@ -39,9 +14,6 @@ const UVSpan = document.createElement("div");
 const linebreak = document.createElement("br");
 const btn1 = document.getElementById("btn1");
 
-//storageArray variable
-// const localStorageString = localStorage.getItem("city");
-// const localStorageArray = JSON.parse(localStorageString);
 let div5 = document.createElement("div");
 
 function addText() {
@@ -78,28 +50,14 @@ let localStorageArray;
     localStorage.setItem("city", JSON.stringify(localStorageArray));
     }
 
-
-// const giveInputButtonValue = function () {
-//     input.value = div5.textContent
-// }
-// div5.addEventListener("click", giveInputButtonValue);
-
-//This is the button that brings the magic to the page! Must be clicked with a city name inside the input in order to work
-
+// This is the button that brings the magic to the page! Must be clicked with a city name inside the input in order to work
 btn1.addEventListener("click", getInput);
-
-//when user clicks on search button, we will want to turn input into string
-//add input value to variable then
-//turn value into a string then use setItem method to add string to local storage
-//take value  from local storage to create button or buttons and make event listener for quick searches
-
 //function that makes the button's magic happen
 function getInput() {
   
   if (document.getElementById("input").value === "") {
     return;
   }
-
   // add a span with the user input inside span
   const div5 = document.createElement("div");
   const userInputSpan = document.createElement("button");
@@ -107,7 +65,6 @@ function getInput() {
   userInputSpan.setAttribute("style", "width: 200px;");
   div2.appendChild(div5);
   div5.prepend(userInputSpan);
-
 
   //variable that get's user input and stores it in the URL for the ajax call
   const city = document.getElementById("input").value;
